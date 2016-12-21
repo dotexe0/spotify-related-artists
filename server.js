0e038f45611fd6136698244e13a92fc3fab422f0
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-12-21 13:43:40 -0800

    added related artists functionality, need to populate it on DOM

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
 
+//   client_id: 'fc5abe67dc6c4af2bf2f917a50f8b361',
+//   client_secret: 'd618fdef87c54bbf843d03523ca6415a'
+
+
 var getFromApi = function(endpoint, args) {
   var emitter = new events.EventEmitter();
-  unirest.get('https://api.spotify.com/v1' + endpoint)
+  unirest.get('https://api.spotify.com/v1/' + endpoint)
          .qs(args)
          .end(function(response) {
             if(response.ok) {
@@ -19,11 +23,6 @@ var getFromApi = function(endpoint, args) {
 var app = express();
 app.use(express.static('public'));
 
-// app.get('/auth', function(req, res) {
-//   client_id: 'fc5abe67dc6c4af2bf2f917a50f8b361',
-//
-// });
-
 app.get('/search/:name', function(req, res) {
   var searchReq = getFromApi('search', {
     q: req.params.name,
@@ -33,8 +32,21 @@ app.get('/search/:name', function(req, res) {
 
   searchReq.on('end', function(item) {
     var artist = item.artists.items[0];
-    res.json(artist);
-  });
+    console.log('artist :', artist);
+
+    var artist_id = item.artists.items[0].id;
+    unirest.get('https://api.spotify.com/v1/artists/'+ artist_id + '/related-artists')
+           .end(function(item) {
+             if (item.ok) {
+               console.log('related artists :', item.body.artists);
+              //  res.json(artist);
+               res.json(item.body.artists);
+             } else {
+               emitter.emit('error', item.code)
+             }
+         });
+        //  res.json(artist);
+      });
 
   searchReq.on('error', function(code) {
     res.sendStatus(code);
